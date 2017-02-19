5ffcac97fa00b698d033a5ff7ee869fbc29951f7
Brad Ballard <ballard6@gmail.com>
2017-02-18 17:23:22 -0800

    Add files via upload

---
+++ supply-demand.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Name PDF that the code will generate
+pp = PdfPages('SupplyAndDemandFloor.pdf')
+# "supply", "demand", "ceiling" and "equilibrium" are all numpy arrays
+# created at the bottom of the script.
+# the supplyAndDemandCeiling method is also called at the bottom of the script
+def supplyAndDemandCeiling(supply, demand, ceiling, equilibrium):
+
+    fig = plt.figure(dpi=128, figsize=(10,6))
+
+    # Plot supply and demand curves
+    # Must instantiate numpy arrays -- see lines 85 and 87
+    plt.plot(supply,  'k-', linewidth=3)
+    plt.plot(demand, 'k-', linewidth=3)
+
+    # The length of the equilibrium line must match the horizontal coordinate
+    # of the equilibrium quanity -- see lines 98, 100
+    plt.plot(equilibrium, 'k--', linewidth=1.5)
+    # The ceiling is a horizontal line -- see lines 90, 94
+    plt.plot(ceiling, 'r--',label= "Price Floor", linewidth=1.5)
+
+    # plt plt takes the form like:
+    # plt.plot((x1,x2)),(y1,y2), 'r-', linewidth = x)
+
+    # e.g., for command on line ##:
+    # create a vertical line at x1 = 2000, y2 = 2000, y1 = 0, y2 = 8000
+    plt.plot((2000, 2000), (0, 8000), 'r--', linewidth=1.5)
+    plt.plot((8000, 8000), (0, 8000), 'r--', linewidth=1.5)
+    plt.plot((5000, 5000), (0, 5000), 'k--', linewidth=1.5)
+
+    # frame = plt.gca() required to use commands for removing
+    # values on axes
+    frame = plt.gca()
+
+    # remove axis values
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
+
+    plt.title('Market', fontsize = 32)
+
+    # p variable on top of y-axis
+    plt.text(-500, 10000, "$p$", fontsize=24)
+
+    # surplus price variable
+    plt.text(-550, 7900, "$p_s$", fontsize=24)
+
+    # equilibrium price variable
+    plt.text(-550, 5000, "$p_e$", fontsize=24)
+
+    # identify supply curve
+    plt.text(8200, 9000,"$S_p$", fontsize = 24)
+    # identify demand curve
+    plt.text(8200, 2000,"$D_p$", fontsize = 24)
+
+
+    # quantity demanded at surplu price
+    plt.text(1800, -650, "$Q_d$", fontsize=24)
+    # quantity supplied at surplus price
+    plt.text(7800, -650, "$Q_s$", fontsize=24)
+    # Qs = Qd at equilibrium price
+    plt.text(4800, -650, "$Q_e$", fontsize=24)
+    # mark that Q is represented on horizontal axis
+    plt.text(10000, -650, "$Q$", fontsize=24)
+
+    # label the axes
+    plt.xlabel("Market ", fontsize=20)
+    plt.ylabel("Price", fontsize = 20)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+#    plt.legend(ceiling, loc='upper right')
+
+    # save PDF of figure
+    # you can also save to image file
+    # pdf is useful if multiple graphs are created
+    pp.savefig(fig)
+
+    # close PDF
+    pp.close()
+
+
+# Instantiate array with supply curve points
+supply = np.arange(10000)
+# ... demand curve points
+demand = np.arange(10000,0, -1)
+
+# ... for price ceiling line
+priceCeiling = np.arange(1, 10000)
+# set value of all points value of price ceiling
+# value of prices are arbitrary since we dropped
+# axis values
+priceCeiling[priceCeiling > 0] = 8000
+
+# ... for equilibrium price, length of this array
+# is equal to the equilibrium quantity
+priceEquilibrium = np.arange(1, 5000)
+# set all values to the equilibrium price
+priceEquilibrium[priceEquilibrium > 0] = 5000
+
+# finally, call method to produce the graph
+supplyAndDemandCeiling(supply,demand, priceCeiling, priceEquilibrium)
+plt.show()
